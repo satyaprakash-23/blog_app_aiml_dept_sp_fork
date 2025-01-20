4f16dc487e629dae5e190ddd425ce7dd2df9fa55
Kumar Harsh <kharsh560@gmail.com>
2025-01-21 01:35:29 +0530

    Finally integrated the "Add Post" feature
    Finally integrated the "Add Post" feature
    
    -> Corrected the backend of "createPost" for browser frontend by properly integrating multer.
    
    -> Created the UI using formData to use the files in req. post method.

--- blog_app_backend/src/routes/post.routes.js
+++ blog_app_backend/src/routes/post.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import verifyJWTandPopulateUserDataInReq from "../middlewares/auth.middleware.js";
 import { createPost, getAllPosts, getPost, getUserPosts } from "../controllers/post.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/createPost").post(verifyJWTandPopulateUserDataInReq, createPost);
+router.route("/createPost").post(verifyJWTandPopulateUserDataInReq, upload.single("poster"), createPost);
 router.route("/getPost/:postId").post(getPost);
 router.route("/getUserPosts").get(verifyJWTandPopulateUserDataInReq, getUserPosts);
 router.route("/getAllPosts").get(getAllPosts);
